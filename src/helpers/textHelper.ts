684854b12cfdce6e94837e8445feda68a0f2eae2
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-09-07 18:19:19 +0200

    unPascalCase as arrow function

--- src/helpers/textHelper.ts
+++ src/helpers/textHelper.ts
@@ -1,5 +1,2 @@
 
-export function unPascalCase(str: string) {
-    // replace all capital letters with a space and lowercase the first letter. The first word will start with a capital letter
-    return str.replace(/([A-Z])/g, " $1").replace(/^./, (str) => str.toUpperCase())
-}
+export const unPascalCase = (str: string) => str.replace(/([A-Z])/g, " $1").replace(/^./, (str) => str.toUpperCase())
