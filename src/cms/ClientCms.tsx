812e64b19ffc17f2808fd0f5e5aa48c0e8886fb5
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-09-07 18:12:13 +0200

    Cms forming :O

--- src/cms/ClientCms.tsx
+++ src/cms/ClientCms.tsx
@@ -1,9 +1,32 @@
+import { Input } from 'antd';
 import React from 'react';
-import { IClientCms } from './types';
-import 'antd/dist/antd.css';
+import { IClientCms, IClientCmsField } from './types';
+import LabelContainer from './LabelContainer';
 
-const ClientCms: React.FC<IClientCms> = <T,>({}: IClientCms<T>) => {
-    return <div className="bg-red-500 max-w-xl h-96"></div>;
+const ClientCms: React.FC<IClientCms> = <T,>({ fields }: IClientCms<T>) => {
+    return (
+        <div className="max-w-xl h-96 flex flex-col gap-y-4 w-full">
+            {fields.map((field, fieldIdx) => (
+                <LabelContainer
+                    key={fieldIdx}
+                    label={field.label || field.name}
+                >
+                    <Component field={field} />
+                </LabelContainer>
+            ))}
+        </div>
+    );
+};
+
+const Component = ({ field }: { field: IClientCmsField }) => {
+    const type = field.type;
+    const props = type.props ? type.props : ({} as any);
+    switch (type.type) {
+        case 'text':
+            return <Input.TextArea {...props} />;
+        default:
+            return <Input className="w-full" />;
+    }
 };
 
 export default ClientCms;
--- src/cms/types.ts
+++ src/cms/types.ts
@@ -1,4 +1,25 @@
+import { TextAreaProps } from "antd/lib/input";
 
 export interface IClientCms<T = unknown> {
     onSubmit?: (data: T) => void;
+    fields: IClientCmsField[];
+}
+
+export interface IClientCmsField {
+    name: string;
+    label: string;
+    type: IClientCmsDefaultType | IClientCmsTextAreaType;
+}
+
+export type IClientCmsDefaultType = {
+    type: "string" | "number" | "boolean" | "date";
+    props?: React.DetailedHTMLProps<
+        React.InputHTMLAttributes<HTMLInputElement>,
+        HTMLInputElement
+    >
+}
+
+export type IClientCmsTextAreaType = {
+    type: "text";
+    props?: TextAreaProps;
 }
