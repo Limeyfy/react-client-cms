08ae467a52c8fc394d98cb3a4b150920620ae873
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-09-17 14:54:58 +0200

    storybook-tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,19 @@
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', {
+    name: '@storybook/addon-postcss',
+    options: {
+      cssLoaderOptions: {
+        // When you have splitted your css over multiple files
+        // and use @import('./other-styles.css')
+        importLoaders: 1,
+      },
+      postcssLoaderOptions: {
+        // When using postCSS 8
+        implementation: require('postcss'),
+      },
+    },
+  }],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
