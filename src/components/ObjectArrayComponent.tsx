2eba3ff6f7db7c7ead1b7736eb4e0920f85ed1aa
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-09-20 09:42:15 +0200

    - unused imports

--- src/components/ObjectArrayComponent.tsx
+++ src/components/ObjectArrayComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Button } from '../Button';
 import { IClientCmsObjectArrayField, LabelContainer } from '../client-cms';
 import { Component } from '../client-cms/CmsComponent';
