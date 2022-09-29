c0c1e72d0c9efb953c980fff30dd5d04af10b7d6
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-09-29 10:12:59 +0200

    readOnly arrays

--- stories/ClientCms.stories.tsx
+++ stories/ClientCms.stories.tsx
@@ -24,8 +24,102 @@ const Template: Story<IClientCms<any>> = args => <ClientCms<any> {...args} />;
 // By passing using the Args format for exported stories, you can control the props for a component for reuse in a test
 // https://storybook.js.org/docs/react/workflows/unit-testing
 export const Default = Template.bind({});
+export const ReadOnly = Template.bind({});
 
 const props: IClientCms<any> = {
+  fields: [
+    {
+      name: 'title',
+    },
+    {
+      name: 'description',
+      type: 'text',
+    },
+    {
+      name: 'total',
+      type: 'number',
+      rules: {
+        required: true,
+        min: 0,
+        max: 100,
+      },
+    },
+    {
+      name: 'selectBox',
+      type: 'select',
+      options: [
+        {
+          value: '1',
+          label: 'One',
+        },
+        {
+          value: '2',
+          label: 'Two',
+        },
+      ],
+
+      renderLabel: (option: any) => option.label,
+      nullValueText: 'Select an option',
+    },
+    {
+      name: 'image',
+      type: 'file',
+    },
+    {
+      name: 'checkbox',
+      type: 'boolean',
+      defaultValue: true,
+    },
+    {
+      name: 'myDate',
+      type: 'date',
+    },
+    {
+      name: 'myCoolObject',
+      type: 'object',
+      fields: [
+        {
+          name: 'subTitle',
+          defaultValue: "I'm a default value",
+        },
+        {
+          name: 'total',
+          type: 'number',
+        },
+      ],
+    },
+    {
+      name: 'myArray',
+      type: 'array',
+      arrayType: 'string',
+      defaultValue: [5032, 2012],
+    },
+    {
+      name: 'myObjectArray',
+      type: 'object-array',
+      fields: [
+        {
+          name: 'description',
+          type: 'text',
+          rows: 3,
+        },
+        {
+          name: 'quantity',
+          type: 'number',
+          defaultValue: 1,
+        },
+        {
+          name: 'price',
+          type: 'number',
+          defaultValue: 250,
+        },
+      ],
+      renderLabel: (item: any) => item.subTitle,
+    },
+  ],
+};
+
+const readOnlyProps: IClientCms<any> = {
   fields: [
     {
       name: 'title',
@@ -129,3 +223,4 @@ const props: IClientCms<any> = {
 };
 
 Default.args = props;
+ReadOnly.args = readOnlyProps;
