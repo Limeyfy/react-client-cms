7f87657403b7bd7835a2305b99bc869c11e8d9bc
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-09-29 10:11:20 +0200

    Empty array states

--- src/components/ArrayComponent.tsx
+++ src/components/ArrayComponent.tsx
@@ -13,6 +13,8 @@ const ArrayComponent: React.FC<IArrayComponentProps> = ({
   arrayType,
   renderLabel,
   disabled,
+  name,
+  label,
 }) => {
   const [items, setItems] = useState<any[]>(value);
   const [text, setText] = useState('');
@@ -41,35 +43,43 @@ const ArrayComponent: React.FC<IArrayComponentProps> = ({
         disabled ? 'bg-gray-100' : 'bg-white'
       )}
     >
-      {items.map((item, index) => (
-        <div className="flex items-center px-3" key={index}>
-          <span className="text-gray-600 py-2 w-full truncate">
-            {renderLabel ? renderLabel(item) : item}
-          </span>
-          {onChange && !disabled && (
-            <button
-              className="p-1 rounded-md transition-all hover:bg-gray-100"
-              onClick={() => !disabled && handleOnRemove(index)}
-              type="button"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-5 h-5 text-red-500"
+      {items.length > 0 ? (
+        items.map((item, index) => (
+          <div className="flex items-center px-3" key={index}>
+            <span className="text-gray-600 py-2 w-full truncate">
+              {renderLabel ? renderLabel(item) : item}
+            </span>
+            {onChange && !disabled && (
+              <button
+                className="p-1 rounded-md transition-all hover:bg-gray-100"
+                onClick={() => !disabled && handleOnRemove(index)}
+                type="button"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                />
-              </svg>
-            </button>
-          )}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5 text-red-500"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </button>
+            )}
+          </div>
+        ))
+      ) : (
+        <div className="w-full h-24 flex justify-center items-center">
+          <p className="text-sm text-gray-600 font-medium">
+            {label ?? name} is empty
+          </p>
         </div>
-      ))}
+      )}
       {onChange && !disabled && (
         <div className="flex items-center justify-between px-3">
           <div className="w-full py-1">
--- src/components/ObjectArrayComponent.tsx
+++ src/components/ObjectArrayComponent.tsx
@@ -18,6 +18,8 @@ const ObjectArrayComponent: React.FC<IObjectArrayComponentProps> = ({
   fields,
   options,
   disabled,
+  name,
+  label,
 }) => {
   const [items, setItems] = useState<any[]>(value);
   const [item, setItem] = useState<any>(getDefaultValueForSimpleFields(fields));
@@ -41,36 +43,44 @@ const ObjectArrayComponent: React.FC<IObjectArrayComponentProps> = ({
 
   return (
     <div className="flex flex-col bg-white border border-gray-300 shadow-md rounded divide-y divide-gray-300">
-      {items.map((item, index) => (
-        <div className="flex items-center px-3" key={index}>
-          <span className="text-gray-600 py-2 w-full truncate">
-            {options?.showItemIndex && `${index}. `}
-            {renderLabel ? renderLabel(item) : item}
-          </span>
-          {onChange && !disabled && (
-            <button
-              className="p-1 rounded-md transition-all hover:bg-gray-100"
-              onClick={() => handleOnRemove(index)}
-              type="button"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-5 h-5 text-red-500"
+      {items.length > 0 ? (
+        items.map((item, index) => (
+          <div className="flex items-center px-3" key={index}>
+            <span className="text-gray-600 py-2 w-full truncate">
+              {options?.showItemIndex && `${index}. `}
+              {renderLabel ? renderLabel(item) : item}
+            </span>
+            {onChange && !disabled && (
+              <button
+                className="p-1 rounded-md transition-all hover:bg-gray-100"
+                onClick={() => handleOnRemove(index)}
+                type="button"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                />
-              </svg>
-            </button>
-          )}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5 text-red-500"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </button>
+            )}
+          </div>
+        ))
+      ) : (
+        <div className="w-full h-24 flex justify-center items-center">
+          <p className="text-sm text-gray-600 font-medium">
+            {label ?? name} is empty
+          </p>
         </div>
-      ))}
+      )}
       {!disabled && (
         <div className="px-4 py-3 flex flex-col gap-y-2">
           {fields.map((f, fIdx) => {
