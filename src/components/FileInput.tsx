19b606f3c925b005f24fb9a4ce4fbb093f165ad6
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-09-30 11:22:46 +0200

    support undefined file

--- src/components/FileInput.tsx
+++ src/components/FileInput.tsx
@@ -73,23 +73,25 @@ export const FileInput: React.FC<IClientCmsFileProps> = props => {
       </Button>
       {props.files && (
         <ul className="mt-2">
-          {Array.from(props.files as any[]).map((file, fileIdx) => (
-            <li key={fileIdx} className="flex justify-between">
-              <span className="truncate w-3/4">
-                {typeof file === 'string' ? file : file.name}
-              </span>
-              <svg
-                onClick={() => removeFile(fileIdx)}
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                className="fill-red-400 cursor-pointer"
-              >
-                <path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path>
-                <path d="M9 10h2v8H9zm4 0h2v8h-2z"></path>
-              </svg>
-            </li>
-          ))}
+          {Array.from(props.files as any[]).map(
+            (file: string | File | undefined, fileIdx) => (
+              <li key={fileIdx} className="flex justify-between">
+                <span className="truncate w-3/4">
+                  {typeof file === 'string' ? file : file?.name}
+                </span>
+                <svg
+                  onClick={() => removeFile(fileIdx)}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  className="fill-red-400 cursor-pointer"
+                >
+                  <path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path>
+                  <path d="M9 10h2v8H9zm4 0h2v8h-2z"></path>
+                </svg>
+              </li>
+            )
+          )}
         </ul>
       )}
       {error && (
